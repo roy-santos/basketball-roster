35cd37140cd63fff893c61f3cb0131325281c562
roy-santos <roysantos112@gmail.com>
2020-11-06 23:05:30 -0800

    Connected mongoDB server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+// mongo connection
+mongoose.connect("mongodb://localhost/basketballRosterDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// body-parser setup (transpile request to something that mongoDB will understand)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send(`Your basketball-roster application is running on port ${PORT}!`);
