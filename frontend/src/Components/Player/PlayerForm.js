96175fd78f9277544e61246ca542b0b0f7e8c034
roy-santos <roysantos112@gmail.com>
2020-12-01 15:03:11 -0800

    Refactor PlayerForm to use createRef() API

--- frontend/src/Components/Player/PlayerForm.js
+++ frontend/src/Components/Player/PlayerForm.js
@@ -1,27 +1,42 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
 
 class PlayerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.speedInput = React.createRef();
+    this.strengthInput = React.createRef();
+    this.enduranceInput = React.createRef();
+    this.abilityInput = React.createRef();
+    this.techniqueInput = React.createRef();
+    this.tacticalInput = React.createRef();
+    this.teamInput = React.createRef();
+    this.coachInput = React.createRef();
+  }
   submitPlayer(event) {
     //event.preventDefault();
 
     axios
-      .post("http://localhost:4000/players", {
-        firstName: this.refs.firstName.value,
-        lastName: this.refs.lastName.value,
-        phone: this.refs.phone.value,
-        email: this.refs.email.value,
-        speed: this.refs.speed.value,
-        strength: this.refs.strength.value,
-        endurance: this.refs.endurance.value,
-        ability: this.refs.ability.value,
-        technique: this.refs.technique.value,
-        tactical: this.refs.tactical.value,
-        team: this.refs.team.value,
-        coach: this.refs.coach.value,
+      .post('http://localhost:4000/players', {
+        firstName: this.firstNameInput.current.value,
+        lastName: this.lastNameInput.current.value,
+        phone: this.phoneInput.current.value,
+        email: this.emailInput.current.value,
+        speed: this.speedInput.current.value,
+        strength: this.strengthInput.current.value,
+        endurance: this.enduranceInput.current.value,
+        ability: this.abilityInput.current.value,
+        technique: this.techniqueInput.current.value,
+        tactical: this.tacticalInput.current.value,
+        team: this.teamInput.current.value,
+        coach: this.coachInput.current.value,
       })
       .then((response) => {
-        console.log(this.refs.coach.value);
+        console.log(this.coachInput.current.value);
         window.location.reload();
       })
       .catch((error) => {
@@ -31,141 +46,146 @@ class PlayerForm extends React.Component {
 
   render() {
     return (
-      <div className="row">
-        <h1 className="center">Add a new player</h1>
-        <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
-          <div className="row">
+      <div className='row'>
+        <h1 className='center'>Add a new player</h1>
+        <form className='col s12' onSubmit={this.submitPlayer.bind(this)}>
+          <div className='row'>
             <h5>Profile</h5>
-            <div className="input-field col s6">
+            <div className='input-field col s6'>
               <input
-                className="firstName"
-                ref="firstName"
-                type="text"
+                className='firstName'
+                ref={this.firstNameInput}
+                type='text'
                 required
               />
-              <label htmlFor="firstName">First Name</label>
+              <label htmlFor='firstName'>First Name</label>
             </div>
-            <div className="input-field col s6">
-              <input id="lastName" ref="lastName" type="text" required />
-              <label htmlFor="lastName">Last Name</label>
+            <div className='input-field col s6'>
+              <input
+                id='lastName'
+                ref={this.lastNameInput}
+                type='text'
+                required
+              />
+              <label htmlFor='lastName'>Last Name</label>
             </div>
-            <div className="input-field col s6">
-              <input id="phone" ref="phone" type="number" required />
-              <label htmlFor="phone">Phone</label>
+            <div className='input-field col s6'>
+              <input id='phone' ref={this.phoneInput} type='number' required />
+              <label htmlFor='phone'>Phone</label>
             </div>
-            <div className="input-field col s6">
-              <input id="email" ref="email" type="email" required />
-              <label htmlFor="email">Email</label>
+            <div className='input-field col s6'>
+              <input id='email' ref={this.emailInput} type='email' required />
+              <label htmlFor='email'>Email</label>
             </div>
             <h5>Attributes</h5>
-            <div className="col s4 ">
-              <label htmlFor="speed">Speed</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='speed'>Speed</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="speed"
-                  ref="speed"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='speed'
+                  ref={this.speedInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
-            <div className="col s4 ">
-              <label htmlFor="strength">Strength</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='strength'>Strength</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="strength"
-                  ref="strength"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='strength'
+                  ref={this.strengthInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
-            <div className="col s4 ">
-              <label htmlFor="endurance">Endurance</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='endurance'>Endurance</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="endurance"
-                  ref="endurance"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='endurance'
+                  ref={this.enduranceInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
-            <div className="col s4 ">
-              <label htmlFor="ability">Ability</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='ability'>Ability</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="ability"
-                  ref="ability"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='ability'
+                  ref={this.abilityInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
-            <div className="col s4 ">
-              <label htmlFor="technique">Technique</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='technique'>Technique</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="technique"
-                  ref="technique"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='technique'
+                  ref={this.techniqueInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
-            <div className="col s4 ">
-              <label htmlFor="tactical">Tactical</label>
-              <p class="range-field">
+            <div className='col s4 '>
+              <label htmlFor='tactical'>Tactical</label>
+              <p className='range-field'>
                 <input
-                  type="range"
-                  id="tactical"
-                  ref="tactical"
-                  min="1"
-                  max="3"
-                  defaultValue="1"
+                  type='range'
+                  id='tactical'
+                  ref={this.tacticalInput}
+                  min='1'
+                  max='3'
+                  defaultValue='1'
                 />
               </p>
             </div>
             <h5>Affiliation</h5>
-            <div className="input-field col s4">
-              <input id="team" ref="team" type="text" required />
-              <label htmlFor="team">Team</label>
+            <div className='input-field col s4'>
+              <input id='team' ref={this.teamInput} type='text' required />
+              <label htmlFor='team'>Team</label>
             </div>
-            <div className="col s12">
+            <div className='col s12'>
               <p>
                 <label>
                   <input
-                    id="coach"
-                    ref="coach"
-                    name="isCoach"
-                    type="radio"
-                    value="true"
+                    id='coach'
+                    ref={this.coachInput}
+                    name='isCoach'
+                    type='radio'
+                    value='true'
                   />
                   <span>Coach</span>
                 </label>
               </p>
               <p>
                 <label>
-                  <input name="isCoach" type="radio" value="false" checked />
+                  <input name='isCoach' type='radio' value='false' checked />
                   <span>Player</span>
                 </label>
               </p>
             </div>
           </div>
           <button
-            className="btn waves-effect waves-light"
-            type="submit"
-            name="action"
+            className='btn waves-effect waves-light'
+            type='submit'
+            name='action'
           >
             Add Player
           </button>
--- frontend/src/Components/Player/PlayerSingle.js
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -1,26 +1,26 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
 const PlayerSingle = (props) => {
   let srcURL;
   if (props.player.firstName === undefined) {
-    srcURL = "images/lakerslogo.jpg";
+    srcURL = 'images/lakerslogo.jpg';
   } else {
     srcURL =
-      "images/" + props.player.firstName + props.player.lastName + ".jpg";
+      'images/' + props.player.firstName + props.player.lastName + '.jpg';
   }
 
   return (
-    <div className="row">
-      <div className="col s12">
-        <div className="card">
-          <div className="card-image">
-            <img src={srcURL} alt="Basketball Player" height="600" />
-            <span className="card-title">
+    <div className='row'>
+      <div className='col s12'>
+        <div className='card'>
+          <div className='card-image'>
+            <img src={srcURL} alt='Basketball Player' height='600' />
+            <span className='card-title'>
               {props.player.firstName} {props.player.lastName}
             </span>
           </div>
-          <div className="card-content">
+          <div className='card-content'>
             <p>
               Phone: {props.player.phone} - Email: {props.player.email}
             </p>
@@ -33,16 +33,17 @@ const PlayerSingle = (props) => {
               {props.player.techniques} - Tactical:{props.player.tactical}
             </p>
           </div>
-          <div className="card-action">Team: {props.player.team}</div>
-          <div className="center">
+          <div className='card-action'>Team: {props.player.team}</div>
+          <div className='center'>
             <button
-              className="btn waves-effect waves-light"
-              type="submit"
-              name="action"
+              className='btn waves-effect waves-light'
+              type='submit'
+              name='action'
             >
               Delete Player
             </button>
           </div>
+          <br />
         </div>
       </div>
     </div>
